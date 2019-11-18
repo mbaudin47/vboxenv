98d7856aec2d496779939a6fe9cfb044cd229df1
Michael BAUDIN <michael.baudin@gmail.com>
2019-11-18 23:21:12 +0100

    Bashrc

---
+++ config_bashrc.sh
@@ -0,0 +1,14 @@
+#export PYTHONPATH="/home/osboxes/GIT/openturns-dev/openturns/build/install/lib/python3.6/site-packages:$PYTHONPATH"
+#export LD_LIBRARY_PATH="/home/osboxes/GIT/openturns-dev/openturns/build/install/lib"
+
+#export COIN_ROOT_DIR="/home/osboxes/Documents/Bonmin-stable/build"
+#export LD_LIBRARY_PATH="/home/osboxes/Documents/Bonmin-stable/build/lib:$LD_LIBRARY_PATH"
+
+#export OPTPP_ROOT_DIR="/home/osboxes/Documents/optpp-2.4/build/install"
+#export OPTPP_LIBRARY="/home/osboxes/Documents/optpp-2.4/build/install/lib"
+#export OPTPP_INCLUDE_DIR="/home/osboxes/Documents/optpp-2.4/build/install/include"
+#export OPTPP_INCLUDE_DIRS="/home/osboxes/Documents/optpp-2.4/build/install/include"
+
+#export http_proxy="http://pcyvipncp2n.edf.fr:3131"
+#export https_proxy="https://pcyvipncp2n.edf.fr:3131"
+
