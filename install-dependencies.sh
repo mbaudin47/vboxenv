50a1fad5d0fa3228bf07b5b45c4d311820c1406e
Michaël BAUDIN <michael.baudin@gmail.com>
2019-11-19 10:54:07 -0500

    Split OT dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -16,17 +16,8 @@ sudo apt-get install -y texlive-base
 sudo apt-get install -y texlive
 sudo apt-get install -y texlive-latex-extra
 sudo apt-get install -y libpng-dev libjpeg-dev 
-sudo apt-get install -y python3-numpy
-sudo apt-get install -y python3-scipy
-sudo apt-get install -y python3-matplotlib
-sudo apt-get install -y python3-nbsphinx 
-sudo apt-get install -y python3-nbconvert
-sudo apt-get install -y python3-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y jupyter-notebook
 sudo apt-get install -y libdlib19 libdlib-dev
 sudo apt-get install -y libceres1 libceres-dev 
 sudo apt-get install -y libsqlite3-0 libsqlite3-dev
-pip3 install numpydoc
-pip3 install -U Sphinx
+
 
---
+++ install-openturns-python-dependencies.sh
@@ -0,0 +1,12 @@
+# Michaël Baudin
+# This script installs OpenTURNS Python dependencies from apt-get
+
+# Python
+sudo apt-get install -y python3-numpy
+sudo apt-get install -y python3-scipy
+sudo apt-get install -y python3-matplotlib
+sudo apt-get install -y python3-nbsphinx 
+sudo apt-get install -y python3-nbconvert
+sudo apt-get install -y python3-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y jupyter-notebook
