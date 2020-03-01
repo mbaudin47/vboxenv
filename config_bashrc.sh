036ce2d05d3e1250daf056efbec9c04851c5d0fd
Michaël BAUDIN <michael.baudin@gmail.com>
2020-03-01 17:16:12 +0100

    Ajoute configuration bash et svn

--- config_bashrc.sh
+++ config_bashrc.sh
@@ -1,11 +1,16 @@
-#export PYTHONPATH="/home/osboxes/GIT/openturns-dev/openturns/build/install/lib/python3.7/site-packages:$PYTHONPATH"
-#export LD_LIBRARY_PATH="/home/osboxes/GIT/openturns-dev/openturns/build/install/lib"
+export PYTHONPATH="/home/osboxes/GIT/openturns-dev/openturns/build/install/lib/python3.7/site-packages:$PYTHONPATH"
+export LD_LIBRARY_PATH="/home/osboxes/GIT/openturns-dev/openturns/build/install/lib"
 
-#export COIN_ROOT_DIR="/home/osboxes/Documents/Bonmin-stable/build"
-#export LD_LIBRARY_PATH="/home/osboxes/Documents/Bonmin-stable/build/lib:$LD_LIBRARY_PATH"
+export COIN_ROOT_DIR="/home/osboxes/Documents/Bonmin-stable/build"
+export LD_LIBRARY_PATH="/home/osboxes/Documents/Bonmin-stable/build/lib:$LD_LIBRARY_PATH"
 
-#export OPTPP_ROOT_DIR="/home/osboxes/Documents/optpp-2.4/build/install"
-#export OPTPP_LIBRARY="/home/osboxes/Documents/optpp-2.4/build/install/lib"
-#export OPTPP_INCLUDE_DIR="/home/osboxes/Documents/optpp-2.4/build/install/include"
-#export OPTPP_INCLUDE_DIRS="/home/osboxes/Documents/optpp-2.4/build/install/include"
+export OPTPP_ROOT_DIR="/home/osboxes/Documents/optpp-2.4/build/install"
+export OPTPP_LIBRARY="/home/osboxes/Documents/optpp-2.4/build/install/lib"
+export OPTPP_INCLUDE_DIR="/home/osboxes/Documents/optpp-2.4/build/install/include"
+export OPTPP_INCLUDE_DIRS="/home/osboxes/Documents/optpp-2.4/build/install/include"
+
+export http_proxy="http://vip-users.proxy.edf.fr:3131"
+export https_proxy="https://vip-users.proxy.edf.fr:3131"
+
+alias rm='echo "Use mv instead"'
 
