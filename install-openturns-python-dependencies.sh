e81e46a550a302c3df0928a8a25bdc6d06b9d43d
Michaël BAUDIN <michael.baudin@gmail.com>
2020-03-17 23:58:43 +0100

    Adding matplotlib, jupyter from pip

--- install-openturns-python-dependencies.sh
+++ install-openturns-python-dependencies.sh
@@ -4,18 +4,21 @@
 set -xe
 
 # Python
-sudo apt install -y python3-numpy
-sudo apt install -y python3-scipy
-sudo apt install -y python3-matplotlib
 sudo apt install -y python3-dev
-sudo apt install -y jupyter-notebook
+#sudo apt install -y jupyter-notebook
 #sudo apt install -y python3-numpydoc
 #sudo apt install -y python3-nbsphinx 
 #sudo apt install -y python3-nbconvert
+#sudo apt install -y python3-numpy
+#sudo apt install -y python3-scipy
+#sudo apt install -y python3-matplotlib
 
 sudo apt-get install python3-pip
 pip3 install nbsphinx
 pip3 install numpydoc
 pip3 install sphinx
 pip3 install pandas
+pip3 install matplotlib
+pip3 install ipython
+pip3 install jupyterlab
 
