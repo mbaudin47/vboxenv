17c21a2a7c9d50c23e08b70aaed2f8f1f199c1cb
Michaël BAUDIN <michael.baudin@gmail.com>
2020-03-17 17:11:22 +0100

    Adding script to download Spectra headers

---
+++ build_spectra.sh
@@ -0,0 +1,9 @@
+# Michaël Baudin
+# This script get and builds Spectra
+
+sudo apt install libeigen3-dev
+
+wget https://github.com/yixuan/spectra/archive/v0.8.1.zip
+mv v0.8.1.zip spectra-v0.8.1.zip
+unzip spectra-v0.8.1.zip
+
