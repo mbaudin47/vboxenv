84571bbff5527a9f479b1eee679ac352919f286d
Michaël BAUDIN <michael.baudin@gmail.com>
2020-03-17 23:14:14 +0100

    Install Sphinx requirements from pip

--- install-openturns-python-dependencies.sh
+++ install-openturns-python-dependencies.sh
@@ -7,9 +7,14 @@ set -xe
 sudo apt install -y python3-numpy
 sudo apt install -y python3-scipy
 sudo apt install -y python3-matplotlib
-sudo apt install -y python3-nbsphinx 
-sudo apt install -y python3-nbconvert
 sudo apt install -y python3-dev
-sudo apt install -y python3-numpydoc
 sudo apt install -y jupyter-notebook
+#sudo apt install -y python3-numpydoc
+#sudo apt install -y python3-nbsphinx 
+#sudo apt install -y python3-nbconvert
+
+sudo apt-get install python3-pip
+pip3 install nbsphinx
+pip3 install numpydoc
+pip3 install sphinx
 
